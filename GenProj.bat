44d1d2a34d0d0389639fb5da60779d78d11c39e2
Vlad-07 <vladup07@gmail.com>
2022-01-30 20:09:36 +0200

    Update GenProj.bat

--- GenProj.bat
+++ GenProj.bat
@@ -1,4 +1,4 @@
-echo !!! Did you go to Eis\vendor and extract spdlog.rar ???
+echo !!! Did you go to Eis\vendor and extract .rar libraries ???
 PAUSE
 call vendor\.bin\premake\premake5.exe vs2022
 PAUSE
