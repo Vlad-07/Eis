b738f12b6b1a8f5450c04c8ef19bc85476a804f3
Vlad-07 <vladup07@gmail.com>
2022-02-07 17:58:20 +0200

    Added required backends to imgui premake file - SAFE

--- Eis/vendor/imgui/premake5.lua
+++ Eis/vendor/imgui/premake5.lua
@@ -19,7 +19,13 @@ project "ImGui"
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
-		"imgui_demo.cpp"
+		"imgui_demo.cpp",
+
+        "backends/imgui_impl_glfw.h",
+        "backends/imgui_impl_glfw.cpp",
+        "backends/imgui_impl_opengl3.h",
+        "backends/imgui_impl_opengl3.cpp",
+        "backends/imgui_impl_opengl3_loader.h"
 	}
 
 	filter "system:windows"
