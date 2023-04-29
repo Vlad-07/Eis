bae4369aedb55ad612afc74d8643101f7129822d
luke <71384528+luke333z@users.noreply.github.com>
2023-04-29 20:20:21 +0300

    Changed Ortho Camera Controller settings
    Added option to lock movement, setters for speeds and position.

--- Eis/src/Eis/Renderer/Objects/OrthoCameraController.cpp
+++ Eis/src/Eis/Renderer/Objects/OrthoCameraController.cpp
@@ -6,44 +6,44 @@
 
 namespace Eis
 {
-	OrthoCameraController::OrthoCameraController(float aspectRatio, bool movement, bool zoom, bool rotation)
-		: m_AspectRatio(aspectRatio), m_lockMovement(movement), m_lockZoom(zoom), m_lockRotation(rotation),
+	OrthoCameraController::OrthoCameraController(float aspectRatio, bool lock, bool rotation)
+		: m_AspectRatio(aspectRatio), m_Rotation(rotation), m_Locked(lock),
 		m_Camera(-m_AspectRatio * m_ZoomLevel, m_AspectRatio * m_ZoomLevel, -m_ZoomLevel, m_ZoomLevel)
 	{
 	}
 
 	void OrthoCameraController::OnUpdate(TimeStep ts)
 	{
-		
-		if (!m_lockMovement)
+		if (m_Locked)
+			return;
+
+		if (Input::IsKeyPressed(EIS_KEY_UP) || Input::IsKeyPressed(EIS_KEY_W))
 		{
-			if (Input::IsKeyPressed(EIS_KEY_UP) || Input::IsKeyPressed(EIS_KEY_W))
-			{
-				m_CameraPosition.x += -sin(glm::radians(m_CameraRotation)) * m_MovementSpeed * ts;
-				m_CameraPosition.y += cos(glm::radians(m_CameraRotation)) * m_MovementSpeed * ts;
-			}
-			if (Input::IsKeyPressed(EIS_KEY_DOWN) || Input::IsKeyPressed(EIS_KEY_S))
-			{
-				m_CameraPosition.x -= -sin(glm::radians(m_CameraRotation)) * m_MovementSpeed * ts;
-				m_CameraPosition.y -= cos(glm::radians(m_CameraRotation)) * m_MovementSpeed * ts;
-			}
-			if (Input::IsKeyPressed(EIS_KEY_LEFT) || Input::IsKeyPressed(EIS_KEY_A))
-			{
-				m_CameraPosition.x -= cos(glm::radians(m_CameraRotation)) * m_MovementSpeed * ts;
-				m_CameraPosition.y -= sin(glm::radians(m_CameraRotation)) * m_MovementSpeed * ts;
-			}
-			if (Input::IsKeyPressed(EIS_KEY_RIGHT) || Input::IsKeyPressed(EIS_KEY_D))
-			{
-				m_CameraPosition.x += cos(glm::radians(m_CameraRotation)) * m_MovementSpeed * ts;
-				m_CameraPosition.y += sin(glm::radians(m_CameraRotation)) * m_MovementSpeed * ts;
-			}
+			m_CameraPosition.x += -sin(glm::radians(m_CameraRotation)) * m_CameraSpeed * ts;
+			m_CameraPosition.y +=  cos(glm::radians(m_CameraRotation)) * m_CameraSpeed * ts;
 		}
-		if (!m_lockRotation)
+		if (Input::IsKeyPressed(EIS_KEY_DOWN) || Input::IsKeyPressed(EIS_KEY_S))
+		{
+			m_CameraPosition.x -= -sin(glm::radians(m_CameraRotation)) * m_CameraSpeed * ts;
+			m_CameraPosition.y -=  cos(glm::radians(m_CameraRotation)) * m_CameraSpeed * ts;
+		}
+		if (Input::IsKeyPressed(EIS_KEY_LEFT) || Input::IsKeyPressed(EIS_KEY_A))
+		{
+			m_CameraPosition.x -= cos(glm::radians(m_CameraRotation)) * m_CameraSpeed * ts;
+			m_CameraPosition.y -= sin(glm::radians(m_CameraRotation)) * m_CameraSpeed * ts;
+		}
+		if (Input::IsKeyPressed(EIS_KEY_RIGHT) || Input::IsKeyPressed(EIS_KEY_D))
+		{
+			m_CameraPosition.x += cos(glm::radians(m_CameraRotation)) * m_CameraSpeed * ts;
+			m_CameraPosition.y += sin(glm::radians(m_CameraRotation)) * m_CameraSpeed * ts;
+		}
+
+		if (m_Rotation)
 		{
 			if (Input::IsKeyPressed(EIS_KEY_Q))
-				m_CameraRotation += m_RotationSpeed * ts;
+				m_CameraRotation += m_CameraRotationSpeed * ts;
 			if (Input::IsKeyPressed(EIS_KEY_E))
-				m_CameraRotation -= m_RotationSpeed * ts;
+				m_CameraRotation -= m_CameraRotationSpeed * ts;
 
 			if (m_CameraRotation > 180.0f)
 				m_CameraRotation -= 360.0f;
@@ -56,22 +56,18 @@ namespace Eis
 
 		m_Camera.SetPosition(m_CameraPosition);
 
-		m_MovementSpeed = m_ZoomLevel; // HACK
+		m_CameraSpeed = m_ZoomLevel; // HACK
 	}
 
 	void OrthoCameraController::OnEvent(Event& e)
 	{
 		EventDispatcher dispatcher(e);
-
 		dispatcher.Dispatch<MouseScrolledEvent>(EIS_BIND_EVENT_FN(OrthoCameraController::OnMouseScrolled));
 		dispatcher.Dispatch<WindowResizeEvent>(EIS_BIND_EVENT_FN(OrthoCameraController::OnWindowResized));
 	}
 
 	bool OrthoCameraController::OnMouseScrolled(MouseScrolledEvent e)
 	{
-		if (m_lockZoom)
-			return false;
-
 		float last = m_ZoomLevel; // HACK: find better way to mantain maximum zoom level acording to sensitivity
 		m_ZoomLevel -= e.GetYOffset() / m_ZoomSensitivity;
 
@@ -80,7 +76,6 @@ namespace Eis
 
 		m_Camera.SetProjection(-m_AspectRatio * m_ZoomLevel, m_AspectRatio * m_ZoomLevel, -m_ZoomLevel, m_ZoomLevel);
 		return false;
-	
 	}
 
 	bool OrthoCameraController::OnWindowResized(WindowResizeEvent e)
--- Eis/src/Eis/Renderer/Objects/OrthoCameraController.h
+++ Eis/src/Eis/Renderer/Objects/OrthoCameraController.h
@@ -10,25 +10,8 @@ namespace Eis
 {
 	class OrthoCameraController
 	{
-	private:
-		float m_AspectRatio;
-		float m_ZoomLevel = 2.0f;
-		
-		OrthographicCamera m_Camera;
-
-		bool m_lockRotation;
-		bool m_lockMovement;
-		bool m_lockZoom;
-
-		glm::vec3 m_CameraPosition = { 0.0f, 0.0f, 0.0f };
-		float m_CameraRotation = 0.0f; //Degrees, anti-clockwise direction
-
-		float m_MovementSpeed = 2.0f,
-			  m_RotationSpeed = 90.0f,
-			  m_ZoomSensitivity = 5.0f;
-		
 	public:
-		OrthoCameraController(float aspectRatio, bool movement=false, bool zoom=false, bool rotation=false);
+		OrthoCameraController(float aspectRatio, bool lock = false, bool rotation = false);
 		~OrthoCameraController() = default;
 
 		inline OrthographicCamera& GetCamera() { return m_Camera; }
@@ -37,35 +20,29 @@ namespace Eis
 		void OnUpdate(TimeStep ts);
 		void OnEvent(Event& e);
 
-		inline glm::vec3 GetPosition() { return m_CameraPosition; }
-		inline void SetPosition(glm::vec3 position) { m_CameraPosition = position; }
+		inline float GetCameraSpeed() { return m_CameraSpeed; }
+		inline void SetCameraSpeed(float speed) { m_CameraSpeed = speed; }
 
-		inline float GetRotation() { return m_CameraRotation; }
-		inline void SetRotation(float rotation) { m_CameraRotation = rotation; }
-
-		inline float GetZoomLevel() { return m_ZoomLevel; }
-		inline void SetZoomLevel(float zoom) { m_ZoomLevel = zoom; }
-
-
-		inline float GetMovementSpeed() { return m_MovementSpeed; }
-		inline void SetMovementSpeed(float speed) { m_MovementSpeed = speed; }
-
-		inline float GetRotationSpeed() { return m_RotationSpeed; }
-		inline void SetRotationSpeed(float speed) { m_RotationSpeed = speed; }
-
-		inline float GetZoomSensitivity() { return m_ZoomSensitivity; }
-		inline void SetZoomSensitivity(float sensitivity) { m_ZoomSensitivity = sensitivity; }
-
-
-		inline void LockMovement(bool state) { m_lockMovement = state; }
-
-		inline void LockRotation(bool state) { m_lockRotation = state; }
-
-		inline void LockZoom(bool state) { m_lockZoom = state; }
-
+		inline float GetSpeedModifier() { return m_SpeedMultiplier; }
+		inline void SetSpeedModifier(float speedMultiplier) { m_SpeedMultiplier = speedMultiplier; }
 
 	private:
 		bool OnMouseScrolled(MouseScrolledEvent e);
 		bool OnWindowResized(WindowResizeEvent e);
+
+	private:
+		float m_AspectRatio;
+		float m_ZoomLevel = 1.0f;
+		float m_SpeedMultiplier = 2.0f;
+		OrthographicCamera m_Camera;
+
+		bool m_Rotation;
+		bool m_Locked;
+
+		glm::vec3 m_CameraPosition = { 0.0f, 0.0f, 0.0f };
+		float m_CameraRotation = 0.0f; //Degrees, anti-clockwise direction
+		float m_CameraSpeed = 2.0f,
+			  m_CameraRotationSpeed = 90.0f,
+		      m_ZoomSensitivity = 5.0f;
 	};
 }
