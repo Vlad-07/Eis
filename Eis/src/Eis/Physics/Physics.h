bf7108452b394d45de4fc708ab5612d9e704e322
Vlad_ <93938455+Vlad-07@users.noreply.github.com>
2026-07-08 19:32:24 +0300

    Removed garbage physics
    Later, Box2D and/or Box3D will be added back.

--- Eis/src/Eis/Physics/Physics.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// BUG: sometimes circles bounce with too much force
-
-#include <Eis/Physics/Objects/Colliders/Collider2D.h>
-#include <Eis/Physics/Objects/Colliders/CircleCollider2D.h>
-#include <Eis/Physics/Objects/Colliders/PolygonCollider2D.h>
-#include <Eis/Physics/Objects/Rigidbody2D.h>
-#include <Eis/Physics/PhysicsManager2D.h>
